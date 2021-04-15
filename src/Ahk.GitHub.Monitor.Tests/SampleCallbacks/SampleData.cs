6ce7e2813971d126721bad8fb43282ded0399bec
Dudas Akos <dudas.akos@vik.bme.hu>
2021-04-15 08:53:08 +0200

    Fix test: sample JSON file line endings should be LF everywhere

--- src/Ahk.GitHub.Monitor.Tests/SampleCallbacks/SampleData.cs
+++ src/Ahk.GitHub.Monitor.Tests/SampleCallbacks/SampleData.cs
@@ -5,7 +5,7 @@ namespace Ahk.GitHub.Monitor.Tests
 {
     internal static class SampleData
     {
-        public static readonly SampleCallbackData BranchCreate = new SampleCallbackData(getTextFromResource("branch_create.json"), "sha1=a3e5f87fb18d2aa2d54ae39510d1a8f1830eab28", "create");
+        public static readonly SampleCallbackData BranchCreate = new SampleCallbackData(getTextFromResource("branch_create.json"), "sha1=ba11ce00817aa44ec7f817561a6ab28e472a1415", "create");
         public static string CommentDelete = getTextFromResource("comment_delete.json");
         public static string CommentEdit = getTextFromResource("comment_edit.json");
         public static string PrOpen = getTextFromResource("pr_open.json");
