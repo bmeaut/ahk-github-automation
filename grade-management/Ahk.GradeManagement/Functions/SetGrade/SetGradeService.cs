ca7690597fcbf522c1d7fdb1f667ff8ea63b4b12
Gergő Radeczki <g.radeczki@gmail.com>
2022-06-10 07:58:16 +0200

    Feature/grademanagement extend (#33)
    * Registered Azure Client
    * Extended SetGrade() to upload results into new queue as well

--- grade-management/Ahk.GradeManagement/Functions/SetGrade/SetGradeService.cs
+++ grade-management/Ahk.GradeManagement/Functions/SetGrade/SetGradeService.cs
@@ -3,19 +3,39 @@ using System.Linq;
 using System.Threading.Tasks;
 using Ahk.GradeManagement.Data;
 using Ahk.GradeManagement.Data.Entities;
+using Azure.Storage.Queues;
+using Microsoft.Extensions.Azure;
 
 namespace Ahk.GradeManagement.SetGrade
 {
     public class SetGradeService : ISetGradeService
     {
+        public const string QueueClientName = "ahkevents";
+        private const string QueueNameSetGrade = "ahk-set-grade";
+        private readonly QueueClient queue;
+        private readonly QueueServiceClient queueService;
         private readonly IResultsRepository repo;
+        private volatile bool queueCreated = false;
 
-        public SetGradeService(IResultsRepository repo)
-            => this.repo = repo;
+        public SetGradeService(IResultsRepository repo, IAzureClientFactory<QueueServiceClient> clientFactory)
+        {
+            this.queueService = clientFactory.CreateClient(QueueClientName);
+            this.queue = queueService.GetQueueClient(QueueNameSetGrade);
+            this.repo = repo;
+        }
 
         public async Task SetGrade(SetGradeEvent data)
         {
             var previousResults = await this.repo.GetLastResultOf(neptun: Normalize.Neptun(data.Neptun), gitHubRepoName: Normalize.RepoName(data.Repository), gitHubPrNumber: data.PrNumber);
+
+            await CreateQueueIfNotExists();
+            await queue.SendMessageAsync(new System.BinaryData(
+                new SetGradeLifecycleEvent(
+                    id: null,
+                    username: data.Repository.Split("-")[^1],
+                    repository: data.Repository,
+                    timestamp: System.DateTime.UtcNow)));
+
             await this.repo.AddResult(new StudentResult(
                 id: null,
                 neptun: data.Neptun,
@@ -58,6 +78,15 @@ namespace Ahk.GradeManagement.SetGrade
             }
 
             return value;
+        }
+
+        private async Task CreateQueueIfNotExists()
+        {
+            if (!queueCreated)
+            {
+                await queue.CreateIfNotExistsAsync();
+                queueCreated = true;
+            }
         }
     }
 }
--- grade-management/Ahk.GradeManagement/Startup.cs
+++ grade-management/Ahk.GradeManagement/Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Azure;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -16,6 +17,7 @@ namespace Ahk.GradeManagement
                 setup.ExpirationScanFrequency = TimeSpan.FromMinutes(4);
             });
 
+            addAzureQueueIntegration(builder);
             builder.Services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
             builder.Services.AddScoped<ResultProcessing.IResultProcessor, ResultProcessing.ResultProcessor>();
             builder.Services.AddScoped<Services.ITokenManagementService, Services.TokenManagementService>();
@@ -28,6 +30,23 @@ namespace Ahk.GradeManagement
             var configValue = new AhkAppConfig();
             configuration.Bind(configValue);
             builder.Services.AddAhkData(configValue.CosmosAccountEndpoint, configValue.CosmosAccountKey);
+        }
+
+        private static void addAzureQueueIntegration(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddAzureClients(az =>
+            {
+                az.ConfigureDefaults(opts => opts.Diagnostics.IsLoggingEnabled = false);
+                az.AddQueueServiceClient(connectionString: Environment.GetEnvironmentVariable("AHK_EventsQueueConnectionString"))
+                    .WithName(SetGrade.SetGradeService.QueueClientName)
+                    .ConfigureOptions(options =>
+                    {
+                        options.MessageEncoding = Azure.Storage.Queues.QueueMessageEncoding.Base64;
+                        options.Retry.Mode = Azure.Core.RetryMode.Exponential;
+                        options.Retry.MaxRetries = 5;
+                        options.Retry.MaxDelay = TimeSpan.FromSeconds(100);
+                    });
+            });
         }
     }
 }
