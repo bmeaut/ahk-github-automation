fb3de5624322c2cc5fa64485fd5ba269f683d8fd
Marton Mack <mackmarton@edu.bme.hu>
2024-03-02 12:09:28 +0100

    Implemented CRUD endpoints for subject

--- grade-management-new/GradeManagement.Server/Controllers/SubjectController.cs
+++ grade-management-new/GradeManagement.Server/Controllers/SubjectController.cs
@@ -1,4 +1,5 @@
-using Microsoft.AspNetCore.Http;
+using GradeManagement.Services.Services;
+using GradeManagement.Shared.DTOs;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GradeManagement.Server.Controllers
@@ -7,5 +8,67 @@ namespace GradeManagement.Server.Controllers
     [ApiController]
     public class SubjectController : ControllerBase
     {
+        SubjectService _subjectService;
+
+        public SubjectController(SubjectService subjectService)
+        {
+            _subjectService = subjectService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<SubjectDTO>>> GetSubjects()
+        {
+            return this.Ok(await _subjectService.GetAllSubjects());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SubjectDTO>> GetSubjectById([FromRoute] long id)
+        {
+            try
+            {
+                return this.Ok(await _subjectService.GetSubjectById(id));
+            }
+            catch (NullReferenceException e)
+            {
+                return this.NotFound(e.Message);
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<SubjectDTO>> UpdateSubject([FromRoute] long id, [FromBody] SubjectDTO subjectDto)
+        {
+            try
+            {
+                return this.Ok(await _subjectService.UpdateSubject(id, subjectDto));
+            }
+            catch (ArgumentException e)
+            {
+                return this.BadRequest(e.Message);
+            }
+            catch (NullReferenceException e)
+            {
+                return this.NotFound(e.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<SubjectDTO>> CreateSubject([FromBody] SubjectDTO subjectDto)
+        {
+            return this.Ok(await _subjectService.CreateSubject(subjectDto));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteSubject([FromRoute] long id)
+        {
+            try
+            {
+                await _subjectService.DeleteSubject(id);
+                return this.NoContent();
+            }
+            catch (NullReferenceException e)
+            {
+                return this.NotFound(e.Message);
+            }
+        }
     }
 }
