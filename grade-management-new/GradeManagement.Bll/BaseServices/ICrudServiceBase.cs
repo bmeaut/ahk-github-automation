7d6429a5da03ef5f0fbc92697c78e8520c4be852
Mack Marton <mackmarton@edu.bme.hu>
2024-03-12 00:14:03 +0100

    Created abstract bases for controllers and services.

---
+++ grade-management-new/GradeManagement.Bll/BaseServices/ICrudServiceBase.cs
@@ -0,0 +1,8 @@
+﻿namespace GradeManagement.Bll.BaseServices;
+
+public interface ICrudServiceBase<DtoClass> : IRestrictedServiceBase<DtoClass>
+{
+
+    public Task<DtoClass> UpdateAsync(long id, DtoClass requestDto);
+
+}
---
+++ grade-management-new/GradeManagement.Server/Controllers/BaseControllers/CrudControllerBase.cs
@@ -0,0 +1,15 @@
+﻿using GradeManagement.Bll.BaseServices;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace GradeManagement.Server.Controllers.BaseControllers;
+
+public abstract class CrudControllerBase<DtoClass>(ICrudServiceBase<DtoClass> crudService)
+    : RestrictedControllerBase<DtoClass>(crudService)
+{
+    [HttpPut("{id:long}")]
+    public async Task<DtoClass> UpdateAsync([FromRoute] long id, [FromBody] DtoClass requestDto)
+    {
+        return await crudService.UpdateAsync(id, requestDto);
+    }
+}
