84409ba6a342fbf4ab1c92c6e67673a20815300d
Zoltán György Gallik <gallikzoltan@gmail.com>
2024-03-18 20:13:11 +0100

    CRUD for subject and courses

---
+++ grade-management-new/GradeManagement.Client/Pages/Endpoints.cs
@@ -0,0 +1,11 @@
+﻿namespace GradeManagement.Client.Pages;
+
+public static class Endpoints
+{
+    public static string SUBJECTS = "api/subjects";
+
+    public static string SEMESTERS = "api/semesters";
+
+    public static string LANGUAGES = "api/languages";
+    public static string COURSES = "api/courses";
+}
