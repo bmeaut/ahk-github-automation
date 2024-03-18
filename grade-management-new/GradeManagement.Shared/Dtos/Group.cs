04e125d8fc967b331ff73fe89dddd062c9615ee7
Marton Mack <mackmarton@edu.bme.hu>
2024-03-18 20:02:46 +0100

    Removed redundant connection between course and Group.
    They are already connected through CourseTeacher entity.

--- grade-management-new/GradeManagement.Shared/Dtos/Group.cs
+++ grade-management-new/GradeManagement.Shared/Dtos/Group.cs
@@ -4,8 +4,6 @@ public class Group
 {
     public long Id { get; set; }
     public string Name { get; set; }
-    public Course Course { get; set; }
-    public long CourseId { get; set; }
     public List<GroupStudent> GroupStudents { get; set; }
     public List<CourseTeacher> CourseTeachers { get; set; }
 }
