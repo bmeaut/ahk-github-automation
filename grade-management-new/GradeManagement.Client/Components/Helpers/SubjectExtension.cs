43950e995f65dc1da78811c3d9c86deb626caee2
Zoltán György Gallik <gallikzoltan@gmail.com>
2024-05-03 13:31:10 +0200

    refactor: variable visibility

--- grade-management-new/GradeManagement.Client/Components/Helpers/SubjectExtension.cs
+++ grade-management-new/GradeManagement.Client/Components/Helpers/SubjectExtension.cs
@@ -4,19 +4,32 @@ namespace GradeManagement.Client.Components.Helpers;
 
 public static class Extensions
 {
-    public static Subject2 ToSubject2(this Subject subject)
+    public static Subject2 ToSubject2(this Subject subject, ICollection<User>? teachers = null)
     {
-        return new Subject2 { Id = subject.Id, Name = subject.Name, NeptunCode = subject.NeptunCode };
+        return new Subject2
+        {
+            Id = subject.Id,
+            Name = subject.Name,
+            NeptunCode = subject.NeptunCode,
+            GitHubOrgName = subject.GitHubOrgName,
+            Teachers = teachers
+        };
     }
 
     public static Subject ToSubject(this Subject2 subject)
     {
-        return new Subject { Id = subject.Id, Name = subject.Name, NeptunCode = subject.NeptunCode };
+        return new Subject
+        {
+            Id = subject.Id,
+            Name = subject.Name,
+            NeptunCode = subject.NeptunCode,
+            GitHubOrgName = subject.GitHubOrgName
+        };
     }
 
-    public static Group2 ToGroup2(this Group group)
+    public static Group2 ToGroup2(this Group group, ICollection<User>? teachers = null)
     {
-        return new Group2 { Id = group.Id, Name = group.Name, CourseId = group.CourseId };
+        return new Group2 { Id = group.Id, Name = group.Name, CourseId = group.CourseId, Teachers = teachers };
     }
 
     public static Group ToGroup(this Group2 group)
@@ -24,11 +37,15 @@ public static class Extensions
         return new Group { Id = group.Id, Name = group.Name, CourseId = group.CourseId };
     }
 
-    public static Student2 ToStudent2(this Student student)
+    public static Student2 ToStudent2(this Student student, ICollection<long>? groupIds = null)
     {
         return new Student2
         {
-            Id = student.Id, Name = student.Name, NeptunCode = student.NeptunCode, GithubId = student.GithubId
+            Id = student.Id,
+            Name = student.Name,
+            NeptunCode = student.NeptunCode,
+            GithubId = student.GithubId,
+            GroupIds = groupIds
         };
     }
 
