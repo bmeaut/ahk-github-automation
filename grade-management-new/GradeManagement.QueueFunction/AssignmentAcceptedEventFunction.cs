197aefcf7a8d04c393b1798940a9aa2f20b19c21
Mack Marton <mackmarton@edu.bme.hu>
2024-05-16 19:06:12 +0200

    Created queue trigger function to consume assignment accepted events.

---
+++ grade-management-new/GradeManagement.QueueFunction/AssignmentAcceptedEventFunction.cs
@@ -0,0 +1,34 @@
+using Azure.Storage.Queues.Models;
+
+using GradeManagement.Bll;
+using GradeManagement.Shared.Dtos.AssignmentEvents;
+
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace GradeManagement.QueueFunction;
+
+public class AssignmentAcceptedEventFunction
+{
+    private readonly ILogger<AssignmentAcceptedEventFunction> _logger;
+    private readonly AssignmentEventProcessorService _assignmentEventProcessorService;
+
+    public AssignmentAcceptedEventFunction(ILogger<AssignmentAcceptedEventFunction> logger,
+        AssignmentEventProcessorService assignmentEventProcessorService)
+    {
+        _logger = logger;
+        _assignmentEventProcessorService = assignmentEventProcessorService;
+    }
+
+    [Function(nameof(AssignmentAcceptedEventFunction))]
+    public async Task Run(
+        [QueueTrigger("assingment-accepted", Connection = "AHK_EventsQueueConnectionString")]
+        AssignmentAccepted assignmentAcceptedEvent)
+    {
+        _logger.LogInformation(
+            $"Assignment accepted event function triggered for repo url: {assignmentAcceptedEvent.GitHubRepositoryUrl}");
+        await _assignmentEventProcessorService.ConsumeAssignmentAcceptedEventAsync(assignmentAcceptedEvent);
+        _logger.LogInformation(
+            $"Assignment accepted event consumed for repo url: {assignmentAcceptedEvent.GitHubRepositoryUrl}");
+    }
+}
