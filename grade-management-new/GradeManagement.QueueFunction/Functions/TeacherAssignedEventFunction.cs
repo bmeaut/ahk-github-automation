159305ea4a480ce5e49bc1d8bc89c4ab0575fb52
Mack Marton <mackmarton@edu.bme.hu>
2024-05-16 19:33:18 +0200

    Minor refactor

--- grade-management-new/GradeManagement.QueueFunction/Functions/TeacherAssignedEventFunction.cs
+++ grade-management-new/GradeManagement.QueueFunction/Functions/TeacherAssignedEventFunction.cs
@@ -6,19 +6,19 @@ using Microsoft.Extensions.Logging;
 
 namespace GradeManagement.QueueFunction.Functions;
 
-public class TeacherAssignedEventEventFunction
+public class TeacherAssignedEventFunction
 {
-    private readonly ILogger<TeacherAssignedEventEventFunction> _logger;
+    private readonly ILogger<TeacherAssignedEventFunction> _logger;
     private readonly AssignmentEventProcessorService _assignmentEventProcessorService;
 
-    public TeacherAssignedEventEventFunction(ILogger<TeacherAssignedEventEventFunction> logger,
+    public TeacherAssignedEventFunction(ILogger<TeacherAssignedEventFunction> logger,
         AssignmentEventProcessorService assignmentEventProcessorService)
     {
         _logger = logger;
         _assignmentEventProcessorService = assignmentEventProcessorService;
     }
 
-    [Function(nameof(TeacherAssignedEventEventFunction))]
+    [Function(nameof(TeacherAssignedEventFunction))]
     public async Task Run(
         [QueueTrigger("teacher-assigned", Connection = "AHK_EventsQueueConnectionString")]
         TeacherAssigned teacherAssigned)
