d7754f5aa2f6a3ebd8d254acfb09b15d46e53354
Mack Marton <mackmarton@edu.bme.hu>
2024-05-17 15:23:27 +0200

    Queue Function working

--- grade-management-new/GradeManagement.QueueFunction/Program.cs
+++ grade-management-new/GradeManagement.QueueFunction/Program.cs
@@ -1,13 +1,37 @@
+using GradeManagement.Bll;
+
 using Microsoft.Azure.Functions.Worker;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
-    .ConfigureServices(services =>
+    .ConfigureServices((context, services) =>
     {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+        services.AddTransient<AssignmentService>();
+        services.AddTransient<AssignmentLogService>();
+        services.AddTransient<AssignmentEventProcessorService>();
+        services.AddTransient<CourseService>();
+        services.AddTransient<GroupService>();
+        services.AddTransient<LanguageService>();
+        services.AddTransient<PullRequestService>();
+        services.AddTransient<ScoreService>();
+        services.AddTransient<SemesterService>();
+        services.AddTransient<StudentService>();
+        services.AddTransient<SubjectService>();
+        services.AddTransient<ExerciseService>();
+        services.AddTransient<UserService>();
+        services.AddTransient<SubjectTeacherService>();
+        services.AddDbContext<GradeManagement.Data.Data.GradeManagementDbContext>(options =>
+        {
+            var connectionString = context.Configuration.GetConnectionString("DbConnection");
+            options.UseSqlServer(connectionString);
+        });
+        services.AddAutoMapper(typeof(GradeManagement.Server.Program).Assembly);
     })
     .Build();
 
