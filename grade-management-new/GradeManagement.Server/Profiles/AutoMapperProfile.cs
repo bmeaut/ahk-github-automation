bc4901f867a8a7631a2158e12d80e0bae5a38510
Mack Marton <mackmarton@edu.bme.hu>
2024-05-21 20:19:41 +0200

    Added auto mapper mappings

--- grade-management-new/GradeManagement.Server/Profiles/AutoMapperProfile.cs
+++ grade-management-new/GradeManagement.Server/Profiles/AutoMapperProfile.cs
@@ -15,7 +15,9 @@ public class AutoMapperProfile : Profile
         CreateMap<Group, Shared.Dtos.Response.Group>();
         CreateMap<Language, Shared.Dtos.Language>();
         CreateMap<PullRequest, Shared.Dtos.PullRequest>();
+        CreateMap<PullRequest, Shared.Dtos.PullRequestForDashboard>();
         CreateMap<Score, Shared.Dtos.Score>();
+        CreateMap<ScoreType, Shared.Dtos.ScoreType>();
         CreateMap<Semester, Shared.Dtos.Semester>();
         CreateMap<Student, Shared.Dtos.Response.Student>();
         CreateMap<Subject, Shared.Dtos.Response.Subject>();
