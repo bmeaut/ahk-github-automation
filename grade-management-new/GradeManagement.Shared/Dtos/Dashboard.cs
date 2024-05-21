ad8937efdc1636640ce718789ef30388aace81eb
Mack Marton <mackmarton@edu.bme.hu>
2024-05-21 20:19:37 +0200

    Created DTOs for dashboard

---
+++ grade-management-new/GradeManagement.Shared/Dtos/Dashboard.cs
@@ -0,0 +1,11 @@
+﻿namespace GradeManagement.Shared.Dtos;
+
+public class Dashboard
+{
+    public string AssignmentName { get; set; }
+    public string GithubRepoUrl { get; set; }
+    public string? StudentNeptun { get; set; }
+    public string ExerciseName { get; set; }
+    public string CourseName { get; set; }
+    public List<PullRequestForDashboard> PullRequests { get; set; }
+}
