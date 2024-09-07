c4d2754093fc0e0dea75933fea19dfdca40c96c6
Mack Marton <mackmarton@edu.bme.hu>
2024-09-07 11:04:38 +0200

    Modified user types

--- grade-management-new/GradeManagement.Shared/Enums/UserType.cs
+++ grade-management-new/GradeManagement.Shared/Enums/UserType.cs
@@ -2,7 +2,7 @@
 
 public enum UserType
 {
-    User = 1,
-    Demonstrator = 2,
-    Teacher = 3
+    User = 1, //Has no special permissions
+    Teacher = 2, //Can create subjects
+    Admin = 3 //Has full control over the system
 }
