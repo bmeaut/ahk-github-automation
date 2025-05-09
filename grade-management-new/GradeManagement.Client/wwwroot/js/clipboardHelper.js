d8183d62cfcbe158a5778426f423fddbad5de645
Marton Mack <mackmarton@edu.bme.hu>
2025-05-09 10:08:58 +0200

    clipboardHelper

---
+++ grade-management-new/GradeManagement.Client/wwwroot/js/clipboardHelper.js
@@ -0,0 +1,8 @@
+window.copyTextFallback = function (text) {
+  const textarea = document.createElement('textarea');
+  textarea.value = text;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand('copy');
+  document.body.removeChild(textarea);
+};
