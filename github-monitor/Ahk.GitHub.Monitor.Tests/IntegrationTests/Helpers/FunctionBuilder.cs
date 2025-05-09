8076201c340a6b59998f8f777801db05f921c6b3
Marton Mack <mackmarton@edu.bme.hu>
2025-05-09 11:43:45 +0200

    Fixed tests

--- github-monitor/Ahk.GitHub.Monitor.Tests/IntegrationTests/Helpers/FunctionBuilder.cs
+++ github-monitor/Ahk.GitHub.Monitor.Tests/IntegrationTests/Helpers/FunctionBuilder.cs
@@ -1,5 +1,7 @@
+using System.Collections.Generic;
 using Ahk.GitHub.Monitor.Config;
 using Ahk.GitHub.Monitor.Services.EventDispatch;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Moq;
@@ -13,6 +15,21 @@ internal static class FunctionBuilder
         GitHubAppId = "appid", GitHubAppPrivateKey = "appprivatekey", GitHubWebhookSecret = "webhooksecret"
     };
 
+
     public static GitHubMonitorFunction Create(IEventDispatchService dispatchService = null)
-        => new(dispatchService ?? new Mock<IEventDispatchService>().Object, Options.Create(AppConfig), new Mock<ILogger<GitHubMonitorFunction>>().Object);
+    {
+        var configValues = new Dictionary<string, string>
+        {
+            { "GitHubMonitorConfig:test:GitHubAppId", AppConfig.GitHubAppId },
+            { "GitHubMonitorConfig:test:GitHubAppPrivateKey", AppConfig.GitHubAppPrivateKey },
+            { "GitHubMonitorConfig:test:GitHubWebhookSecret", AppConfig.GitHubWebhookSecret }
+        };
+
+        var configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(configValues)
+            .Build();
+
+        return new GitHubMonitorFunction(
+            dispatchService ?? new Mock<IEventDispatchService>().Object, new Mock<ILogger<GitHubMonitorFunction>>().Object, configuration);
+    }
 }
